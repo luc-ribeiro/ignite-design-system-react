294528ccf8a1e17e5af8c034af9acc2f77d53f22
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-03-03 14:44:09 -0300

    ci: Add docs workflow

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -16,5 +16,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  }
 }
 export default config
